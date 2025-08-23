f71f4ae9227d52a1e015f6482ec871759f6e5549
wildanfh <wildanf2005@gmail.com>
2025-08-23 10:58:24 +0200

    object, function, etc. annotations type

--- index.js
+++ index.js
@@ -6,3 +6,58 @@ var myNumberExOne = 42;
 var myStringExOne = "Hello Typescript";
 console.log("My Number Is", myNumberExOne);
 console.log("My String Is", myStringExOne);
+var greeting = "Hello, world!";
+console.log(greeting);
+var myVariable = 42;
+myVariable = "Hello, TypeScript";
+console.log(myVariable);
+// Type Annotations With Objects
+var personExOne;
+personExOne = {
+    name: "Wildan",
+    age: 20,
+    jobTitle: "Software Developer",
+    address: {
+        street: "Am Rollberg 40",
+        city: "Berlin",
+    },
+};
+// another method. Define an object type with type Annotations
+var personExTwo = {
+    name: "Wildan",
+    age: 20,
+    jobTitle: "Software Developer",
+    address: {
+        street: "Am Rollberg 40",
+        city: "Berlin",
+    },
+};
+// Annotations with function. Function to calculate the area of a rectangle.
+function calculateRectangleArea(long, width) {
+    return long * width;
+}
+;
+// Calling the function with valid arguments
+var long = 5;
+var width = 4;
+var area = calculateRectangleArea(long, width);
+console.log("The area of the rectangle is : ".concat(area));
+// Optional And Default Parameter
+function greetOptional(name, age) {
+    if (age !== undefined) {
+        console.log("Hello, ".concat(name, "! You are ").concat(age, " years old"));
+    }
+    else {
+        console.log("Hello, ".concat(name, "!"));
+    }
+}
+;
+greetOptional("Wildan");
+greetOptional("Dzaki", 12);
+// Default Parameters:
+function greetDefault(name, age) {
+    if (age === void 0) { age = 20; }
+    console.log("Hello, ".concat(name, "! You are ").concat(age, " years old."));
+}
+greetDefault("Izza");
+greetDefault("Rifka", 18);
--- index.ts
+++ index.ts
@@ -10,3 +10,88 @@ let myStringExOne: string = "Hello Typescript";
 console.log("My Number Is", myNumberExOne);
 console.log("My String Is", myStringExOne);
 
+let greeting = "Hello, world!";
+console.log(greeting);
+
+let myVariable : any = 42;
+myVariable = "Hello, TypeScript";
+console.log(myVariable);
+
+
+// Type Annotations With Objects
+let personExOne : {
+  name : string;
+  age : number;
+  jobTitle? : string;
+  address : {
+    street : string;
+    city: string;
+  };
+};
+
+personExOne = {
+  name : "Wildan",
+  age : 20,
+  jobTitle: "Software Developer",
+  address : {
+    street : "Am Rollberg 40",
+    city : "Berlin",
+  },
+};
+
+// another method. Define an object type with type Annotations
+
+let personExTwo: {
+  name: string;
+  age: number;
+  jobTitle?: string;
+  address: {
+    street: string;
+    city: string;
+  };
+} = {
+  name : "Wildan",
+  age : 20,
+  jobTitle: "Software Developer",
+  address : {
+    street : "Am Rollberg 40",
+    city : "Berlin",
+  },
+};
+
+// Annotations with function. Function to calculate the area of a rectangle.
+
+function calculateRectangleArea(long : number, width : number) : number {
+  return long * width;
+};
+
+// Calling the function with valid arguments
+
+const long = 5;
+const width = 4;
+const area = calculateRectangleArea(long, width);
+console.log(`The area of the rectangle is : ${area}`);
+
+// Optional And Default Parameter
+
+function greetOptional(name : string, age? : number) {
+  if (age !== undefined) {
+    console.log(`Hello, ${name}! You are ${age} years old`);
+  } else {
+    console.log(`Hello, ${name}!`);
+  }
+};
+
+greetOptional("Wildan");
+greetOptional("Dzaki", 12);
+
+// Default Parameters:
+function greetDefault(name: string, age: number = 20) {
+  console.log(`Hello, ${name}! You are ${age} years old.`);
+}
+
+greetDefault("Izza");
+greetDefault("Rifka", 18);
+
+
+
