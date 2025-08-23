166eac9d7299e123433a88713c0f1852eb68c677
wildanfh <wildanf2005@gmail.com>
2025-08-24 00:17:36 +0200

    learn object, function, type-system-concepts, array, tuple, enums dan interface in typescript

--- index.js
+++ index.js
@@ -61,3 +61,212 @@ function greetDefault(name, age) {
 }
 greetDefault("Izza");
 greetDefault("Rifka", 18);
+// Function with a rest parameter and type annotations
+function addAll() {
+    var nums = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        nums[_i] = arguments[_i];
+    }
+    var result = 0;
+    for (var i = 0; i < nums.length; i++) {
+        result += nums[i];
+    }
+    return result;
+}
+;
+// Calling the function with various numeric arguments
+console.log(addAll(10, 20, 30, 100, 10.5, +true));
+// Arrow function
+var addWithArrow = function (num1, num2) { return num1 + num2; };
+console.log(addWithArrow(5, 10));
+// Anonymous function
+var add = function (num1, num2) {
+    return num1 + num2;
+};
+console.log(add(20, 30));
+// void & never
+// void
+function greet(name) {
+    console.log("Hello, ".concat(name, "!"));
+}
+;
+function noReturnValue() {
+    // This function doesn't return a value (implicity returns undefined).
+}
+;
+// never
+function ThrowError(message) {
+    throw new Error(message);
+}
+;
+function infiniteLoop() {
+    while (true) {
+        // This function never exits (infinite loop).
+    }
+}
+;
+// Union Type
+var myVar;
+myVar = "Moin!"; // Valid assignment
+console.log(myVar);
+myVar = 20; // Valid assignment
+console.log(myVar);
+// Literal Types
+var direction;
+// In function parameter
+function setColor(color) {
+    // ...
+}
+;
+// Nullable Types
+var username = "Anies"; // The variable can hold a string or null
+var age = null; // The variable can hold a number or null
+function greetUser(username) {
+    if (username === void 0) { username = null; }
+    if (username === null) {
+        console.log("Assalamualaikum Habibi");
+    }
+    else {
+        console.log("Hello, ".concat(username, "!"));
+    }
+}
+;
+greetUser("Anies"); // Output: Hello, Anies!
+greetUser(null); // Output: Assalamualaikum, Habibi!
+greetUser();
+function greetUserbyTime(username) {
+    var hour = new Date().getHours();
+    var waktu;
+    if (hour < 12) {
+        waktu = "pagi";
+    }
+    else if (hour < 18) {
+        waktu = "siang";
+    }
+    else {
+        waktu = "malam";
+    }
+    if (username == null || username.trim() === "") {
+        console.log("Assalamualaikum Habibi, selamat ".concat(waktu, " \uD83C\uDF38"));
+    }
+    else {
+        console.log("Assalamualaikum ".concat(username, ", selamat ").concat(waktu, "!"));
+    }
+}
+// Contoh pemakaian
+greetUserbyTime("Wildan"); // Assalamualaikum Wildan, selamat pagi!
+greetUserbyTime(null); // Assalamualaikum Habibi, selamat pagi 🌸
+greetUserbyTime(); // Assalamualaikum Habibi, selamat pagi 🌸
+// Use the type alias for string
+var myName = "Wildan";
+// Use the type alias for the union type
+var myValue = 10;
+var wildan = {
+    name: "Wildan",
+    age: 20,
+    email: "wildanf2005@gmail.com",
+};
+var rifka = {
+    name: "Rifka",
+    age: 18,
+};
+console.log(wildan);
+console.log(rifka);
+var jono = {
+    name: "Jono",
+    age: 30,
+    email: "jono@example.com",
+};
+var indro = {
+    name: "Indro",
+    age: 25,
+};
+console.log(jono);
+console.log(indro);
+var person = {
+    name: 'John Doe',
+    age: 27,
+    address: '123 Main St',
+    phone: '123-456-799',
+};
+// Type Annotations With Arrays
+// Annotating an array of strings
+var fruits = ["apple", "banana", "cherry"];
+// Using a loop to process each element in the array
+for (var i = 0; i < fruits.length; i++) {
+    console.log("Fruit : ".concat(fruits[i].toUpperCase()));
+}
+// Type Annotations With Multidimensional Arrays
+var arrayOne = [1, 2, 3, 4, 5];
+var arrayTwo = ["A", "B", "C"];
+var arrayThree = [1, 2, 3, 4, "A", "B", "C"];
+// Tuple
+// Defining a tuple type
+var article = [11, "Title One", true];
+// Assigning a new tuple value to the `article` variable
+article = [12, "Title Two", false];
+// Trying to push a new element into a tuple (will result in an error)
+// article.push(100);
+console.log(article);
+// Destructuring the tuple into individual variables
+var id = article[0], title = article[1], published = article[2];
+console.log(id);
+console.log(title);
+console.log(published);
+// Enums
+var Tage;
+(function (Tage) {
+    Tage[Tage["Sonntag"] = 0] = "Sonntag";
+    Tage[Tage["Montag"] = 1] = "Montag";
+    Tage[Tage["Dienstag"] = 2] = "Dienstag";
+    Tage[Tage["Mittwoch"] = 3] = "Mittwoch";
+    Tage[Tage["Donnerstag"] = 4] = "Donnerstag";
+    Tage[Tage["Freitag"] = 5] = "Freitag";
+    Tage[Tage["Samstag"] = 6] = "Samstag";
+})(Tage || (Tage = {}));
+var today = Tage.Samstag;
+console.log("Today is ".concat(Tage[today]));
+var John = {
+    name: "John",
+    age: 30,
+};
+// Create an object that adheres to the 'Person' interface
+var Sarah = {
+    name: "Sarah",
+    age: 25,
+    greet: function (message) {
+        console.log("".concat(this.name, " says : ").concat(message));
+    },
+};
+Sarah.greet("Hello, TypeScript!"); // Output: Sarah says: Hello, TypeScript!
+// Create an object 'userSettings' that adheres to the merged 'Settings' interface
+var userSettings = {
+    theme: true,
+    font: "Open Sans",
+    sidebar: false,
+    external: true,
+};
+var Labrador = /** @class */ (function () {
+    function Labrador() {
+        this.type = "dog";
+    }
+    Labrador.prototype.bark = function () {
+        console.log("Woof!");
+    };
+    return Labrador;
+}());
+var Beagle = /** @class */ (function () {
+    function Beagle() {
+    }
+    Beagle.prototype.bark = function () {
+        console.log("Woof!");
+    };
+    return Beagle;
+}());
+var labrador = {
+    type: "monkey",
+    bark: function () {
+        console.log("Woof!");
+    },
+};
+labrador.bark();
--- index.ts
+++ index.ts
@@ -93,5 +93,390 @@ function greetDefault(name: string, age: number = 20) {
 greetDefault("Izza");
 greetDefault("Rifka", 18);
 
+// Function with a rest parameter and type annotations
+function addAll(...nums : number[]) : number {
+  let result = 0;
+  for (let i = 0; i < nums.length; i++) {
+    result += nums[i];
+  }
+  return result;
+};
 
+// Calling the function with various numeric arguments
+console.log(addAll(10, 20, 30, 100, 10.5, +true));
+
+// Arrow function
+const addWithArrow = (num1 : number, num2 : number) : number => num1 + num2;
+console.log(addWithArrow(5, 10));
+
+// Anonymous function
+const add = function(num1 : number, num2 : number) : number {
+  return num1 + num2;
+};
+console.log(add(20, 30));
+
+// void & never
+// void
+function greet(name : string) : void {
+  console.log(`Hello, ${name}!`);
+};
+
+function noReturnValue() : void {
+  // This function doesn't return a value (implicity returns undefined).
+};
+
+// never
+function ThrowError(message : string) : never {
+  throw new Error(message);
+};
+
+function infiniteLoop() : never {
+  while (true) {
+    // This function never exits (infinite loop).
+  }
+};
+
+// Union Type
+let myVar : string | number;
+
+myVar = "Moin!"; // Valid assignment
+console.log(myVar);
+
+myVar = 20; // Valid assignment
+console.log(myVar);
+
+// Literal Types
+let direction : "left" | "right" | "up" | "down";
+
+// In function parameter
+function setColor(color : "red" | "green" | "blue") {
+  // ...
+};
+
+// Nullable Types
+let username : string | null = "Anies"; // The variable can hold a string or null
+
+let age : number | null = null; // The variable can hold a number or null
+
+function greetUser(username : string | null = null) {
+  if(username === null) {
+    console.log("Assalamualaikum Habibi");
+  } else {
+    console.log(`Hello, ${username}!`);
+  }
+};
+
+greetUser("Anies"); // Output: Hello, Anies!
+greetUser(null);     // Output: Assalamualaikum, Habibi!
+greetUser();
+
+function greetUserbyTime(username?: string | null) {
+  const hour = new Date().getHours();
+  let waktu: string;
+
+  if (hour < 12) {
+    waktu = "pagi";
+  } else if (hour < 18) {
+    waktu = "siang";
+  } else {
+    waktu = "malam";
+  }
+
+  if (username == null || username.trim() === "") {
+    console.log(`Assalamualaikum Habibi, selamat ${waktu} 🌸`);
+  } else {
+    console.log(`Assalamualaikum ${username}, selamat ${waktu}!`);
+  }
+}
+
+// Contoh pemakaian
+greetUserbyTime("Wildan"); // Assalamualaikum Wildan, selamat pagi!
+greetUserbyTime(null);     // Assalamualaikum Habibi, selamat pagi 🌸
+greetUserbyTime();         // Assalamualaikum Habibi, selamat pagi 🌸
+
+// Type Alias
+// Define a type alias for string
+type MyString = string;
+
+// Use the type alias for string
+let myName : MyString = "Wildan";
+
+// Define a type alias for a union of string or number
+type MyStringOrNumber = string | number;
+
+// Use the type alias for the union type
+let myValue : MyStringOrNumber = 10;
+
+// with object
+
+type Person = {
+  name : string;
+  age : number;
+  email? : string;
+}
+
+const wildan : Person = {
+  name : "Wildan",
+  age : 20,
+  email : "wildanf2005@gmail.com",
+};
+
+const rifka: Person = {
+  name: "Rifka",
+  age: 18,
+};
+
+console.log(wildan);
+console.log(rifka);
+
+// another example
+type Employee = {
+  name : string;
+  age : number;
+  email? : string;
+};
+
+const jono : Employee = {
+  name: "Jono",
+  age: 30,
+  email: "jono@example.com",
+};
+
+const indro : Employee = {
+  name: "Indro",
+  age: 25,
+};
+
+console.log(jono);
+console.log(indro);
+
+// Intersection Type
+type FirstType = {
+  name : string;
+  age : number;
+};
+
+type SecondType = {
+  address : string;
+  phone : string;
+};
+
+type CombinedType = FirstType & SecondType;
+
+const person : CombinedType = {
+  name : 'John Doe',
+  age : 27,
+  address : '123 Main St',
+  phone : '123-456-799',
+};
+
+// Type Annotations With Arrays
+
+// Annotating an array of strings
+let fruits : string[] = ["apple", "banana", "cherry"];
+
+// Using a loop to process each element in the array
+for (let i = 0; i < fruits.length; i++) {
+  console.log(`Fruit : ${fruits[i].toUpperCase()}`);
+}
+
+// Type Annotations With Multidimensional Arrays
+let arrayOne : number[] = [1, 2, 3, 4, 5];
+let arrayTwo : string[] = ["A", "B", "C"];
+let arrayThree : (string | number)[] = [1, 2, 3, 4, "A", "B", "C"];
+
+// Tuple
+
+// Defining a tuple type
+let article : readonly [number, string, boolean] = [11, "Title One", true];
+
+// Assigning a new tuple value to the `article` variable
+article = [12, "Title Two", false];
+
+// Trying to push a new element into a tuple (will result in an error)
+// article.push(100);
+
+console.log(article);
+
+// Destructuring the tuple into individual variables
+const [id, title, published] = article;
+console.log(id);
+console.log(title);
+console.log(published);
+
+// Enums
+
+enum Tage {
+  Sonntag,
+  Montag,
+  Dienstag,
+  Mittwoch,
+  Donnerstag,
+  Freitag,
+  Samstag
+}
+
+const today: Tage = Tage.Samstag;
+
+console.log(`Today is ${Tage[today]}`);
+
+// Interface
+
+interface PersonEx1 {
+  name : string;
+  age : number;
+}
+
+const John : PersonEx1 = {
+  name : "John",
+  age : 30,
+};
+
+// Interface Method And Parameters
+
+// Define an interface 'Person' with a method 'greet'
+interface PersonEx2 {
+  name : string;
+  age : number;
+  greet(message : string) : void;
+}
+
+// Create an object that adheres to the 'Person' interface
+const Sarah : PersonEx2 = {
+  name : "Sarah",
+  age : 25,
+  greet(message : string) {
+    console.log(`${this.name} says : ${message}`);
+  },
+};
+
+Sarah.greet("Hello, TypeScript!"); // Output: Sarah says: Hello, TypeScript!
+
+// ReOpen The Interface And Use Cases
+
+// Homepage Interface Declaration
+interface Settings {
+  readonly theme : boolean;
+  font : string;
+}
+
+// Articles Page Interface Reopening
+interface Settings {
+  sidebar : boolean;
+}
+
+// Contact Page Interface Reopening
+interface Settings {
+  external : boolean;
+}
+
+// Create an object 'userSettings' that adheres to the merged 'Settings' interface
+let userSettings : Settings = {
+  theme : true,
+  font : "Open Sans",
+  sidebar : false,
+  external : true,
+};
+
+// built-in Interface
+// const imgElement : HTMLImageElement = document.createElement("img");
+// imgElement.src = "image.jpeg";
+// imgElement.alt = "My Image";
+
+// if (imgElement.complete) {
+//   console.log(`Image dimensions : ${imgElement.naturalWidth}x${imgElement.naturalHeight}`);
+// } else {
+//   imgElement.addEventListener("load", () => {
+//     console.log(`Image dimensions : ${imgElement.naturalWidth}x${imgElement.naturalHeight}`);
+//   });
+// }
+
+// Interface vs. Type Aliases
+
+// 1. Declaration Syntax
+interface PersonExInterface {
+  name : string;
+  age : number;
+}
+
+type PersonExType = {
+  name : string;
+  age : number;
+};
+interface AnimalExInterface {
+  type : string;
+}
+
+interface Dog extends AnimalExInterface {
+  bark() : void;
+}
+
+class Labrador implements Dog {
+  type : string = "dog";
+  bark() {
+    console.log("Woof!");
+  }
+}
+
+// 2. Extending and Implementing:
+
+interface Animal {
+  type : string;
+}
+
+interface Dog extends Animal {
+  bark() : void;
+}
+
+class Beagle implements Dog {
+  type : "dog";
+  bark() {
+    console.log("Woof!");
+  }
+}
+
+type AnimalExType = {
+  type : string;
+}
+
+type Monkey = AnimalExType & {
+  bark() : void;
+};
+
+const labrador : Monkey = {
+  type : "monkey",
+  bark() {
+    console.log("Woof!");
+  },
+};
+
+labrador.bark();
+
+// 3. Compability
+interface Car {
+  brand : string;
+}
+
+interface Car {
+  model : string;
+}
+
+const myCar : Car = {
+  brand : "Toyota",
+  model : "Camry",
+};
+
+// Type Alias: Does not support declaration merging.
+
+// 4. Extensibility:
+
+// Interface: Use when you need to define the shape of objects and also when you need to implement them in classes. They are more suitable for defining contracts that classes must adhere to.
+// Type Alias: Use when you want to create custom types that can be combined using union types, intersection types, or when defining complex types.
+
+// 5. Readability and Style:
+
+// Interface: Preferred when defining shapes of objects, especially for public APIs, as they convey the intent clearly. They also work well with code editors' IntelliSense for auto-completion and documentation generation.
+// Type Alias: Useful for defining unions, intersections, and more complex types. They are often used to give descriptive names to specific combinations of types.
+
+// Class Type annotations
 
